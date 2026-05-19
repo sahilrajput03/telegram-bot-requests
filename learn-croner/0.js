36af1c9395a706f2cd725f35def3a8d5e6537967
Sahil Rajput <sahilrajput03@gmail.com>
2026-05-20 00:37:12 +0530

    Update.

--- learn-croner/0.js
+++ learn-croner/0.js
@@ -2,7 +2,8 @@ const { Cron, scheduledJobs } = require('croner');
 
 // Web: https://croner.56k.guru
 // Docs: https://github.com/Hexagon/croner
-// Example: https://chatgpt.com/c/6a0c9837-c090-83e8-aabc-7212219c85be
+// ChatGPT: https://chatgpt.com/c/6a0c9837-c090-83e8-aabc-7212219c85be
+// Deepwiki: https://deepwiki.com/search/i-want-to-load-cronjobs-from-a_dca296ea-5665-45bd-8067-f34cf5eea214?mode=fast
 
 
 // Example of creating cronjob:
