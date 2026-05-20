b1a119c18ef91b0370dd58240efde4b0dbd6e8e7
Sahil Rajput <sahilrajput03@gmail.com>
2026-05-20 13:07:11 +0530

    Update.

--- learn-croner/1.js
+++ learn-croner/1.js
@@ -3,9 +3,11 @@ const { Cron, scheduledJobs } = require('croner');
 const { app1DB } = require('./filedb');
 
 const defaultOptns = { timezone: 'Asia/Kolkata' };
-
-const aauJobs = app1DB.get().agents?.aau?.jobs || [];
-// Shape of a job: `{name, pattern}`
+const workProgresJob = {
+    name: 'WORK_PROGRESS_CHECK',
+    pattern: '*/5 * * * * *',
+    function: () => console.log(`Running work check job (every fifth second)`)
+};
 
 // & What do I want to do?
 // [x] Be able to add a cronjob at runtime using setTimeout in this
@@ -15,24 +17,22 @@ const aauJobs = app1DB.get().agents?.aau?.jobs || [];
 //    `scheduledJobs` on program start to prevent adding unless already
 //    added.
 
-const workProgresJob = {
-    name: 'WORK_PROGRESS_CHECK',
-    pattern: '*/5 * * * * *',
-    function: () => console.log(`Running work check job (every fifth second)`)
-};
-
-// Load and start jobs from file  
-aauJobs.forEach(j => {
-    new Cron(j.pattern, { name: j.name, ...defaultOptns }, workProgresJob.function);
-});
+// Shape of a job: `{name, pattern}`
+const aauJobs = app1DB.get().agents?.aau?.jobs || [];
+if (aauJobs.length > 0) {
+    // Load and start jobs from file  
+    aauJobs.forEach(j => {
+        new Cron(j.pattern, { name: j.name, ...defaultOptns }, workProgresJob.function);
+    });
+    console.log("✅ Loaded aau's jobs.");
+}
 
 // If job not found then add job dynamically after 2 secs
-if (!scheduledJobs.map(j => j.name).find(name => name === "WORK_PROGRESS_CHECK")) {
-    console.log('🚀 `workProgresJob` not found. Scheduled to be ran in 2 seconds.');
-    setTimeout(() => {
-        const job = new Cron(workProgresJob.pattern, { name: workProgresJob.name, ...defaultOptns }, workProgresJob.function);
-        // Add cronjob to fileDB
-        app1DB.update(db => { db.agents?.aau?.jobs.push(workProgresJob); });
-        console.log('✅ Added job successfully.');
-    }, 2_000);
+const notFoundInScheduledJobs = !scheduledJobs.map(j => j.name).find(name => name === "WORK_PROGRESS_CHECK");
+if (notFoundInScheduledJobs) {
+    console.log('🚀 No `workProgresJob` found.');
+    const job = new Cron(workProgresJob.pattern, { name: workProgresJob.name, ...defaultOptns }, workProgresJob.function);
+    // Add cronjob to fileDB
+    app1DB.update(db => { db.agents?.aau?.jobs.push(workProgresJob); });
+    console.log('✅ Created `workProgresJob` & updated to `fileDB.json`.');
 }
