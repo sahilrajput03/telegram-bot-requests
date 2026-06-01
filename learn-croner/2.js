672cc5489b875f7ed7bb216ce566e04dc1a29f3f
Sahil Rajput <sahilrajput03@gmail.com>
2026-06-01 18:25:45 +0530

    Update.

--- learn-croner/2.js
+++ learn-croner/2.js
@@ -6,9 +6,10 @@ console.log('🚀Program Started:', getHumanReadableIndianTime());
 // Every 2 hours from 12pm to 6pm: 0 0 12-18/2 * * 1-5
 
 // Every 2 minutes from 12min to 42min
-const job = new Cron('12-42/2 * * * 1-5', { name: 'WORK_PROGRESS_CHECK' }, () => {
+const job = new Cron("0-12/3 18 1 * *", { name: 'WORK_PROGRESS_CHECK' }, () => {
     console.log('Every 2 minutes from 12min to 42min');
     console.log('CRON: WORK_PROGRESS_CHECK:', getHumanReadableIndianTime());
 });
 console.log("🚀 ~ job.name?", job.name);
 console.log("🚀 ~ scheduledJobs?", scheduledJobs.map(c => c.name));
+
